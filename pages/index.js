dcb5d7d8415b53ccaae5beb3f4aa61b745ecf94f
Caio Lucas da Silva <clsdraco@gmail.com>
2021-01-25 12:33:35 -0300

    Dia 1 - Commit Inicial

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,42 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import BackgroundImage from '../src/components/BackgroundImage';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage backgroundImage={db.bg} >
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The Legend of Zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem impsum dolor met it...</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>The Legend of Zelda</h1>
+            <p>lorem impsum dolor met it...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="//github.com/clskayyo" />
+    </BackgroundImage>
+  );
 }
